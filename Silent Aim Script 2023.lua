2bd978f82d5d25fcac963163213236656489ccce
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2025-03-30 13:59:52 +0200

    Add files via upload

---
+++ Silent Aim Script 2023.lua
@@ -0,0 +1,8 @@
+shared.config = {
+    hitPart = "Head", -- Head, Neck, Chest, Abdomen
+    fieldOfView = 120,
+    teamCheck = true,
+    circleColor = Color3.fromRGB(31, 237, 59)
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xml4/luau/main/games/badbusiness/silentaim.lua"))()
